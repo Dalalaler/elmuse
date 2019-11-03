ca0c693ce2ff79ff484f998115d34ebdddec172a
repatik <dalalaler@gmail.com>
2019-11-04 02:06:48 +0300

    Added dynaconf

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
+
+#import dj_database_url
+#from django.urls import reverse_lazy
+from dynaconf import settings as _settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +25,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!zjsazu@jcsj2jo-m@3jd2jd+uqx7&ct8yqd%78zqi+14)vn%x'
+SECRET_KEY = _settings.SECRET_KEY
+#SECRET_KEY = '!zjsazu@jcsj2jo-m@3jd2jd+uqx7&ct8yqd%78zqi+14)vn%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = _settings.DEBUG
+#DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'elmusetest.herokuapp.com',]
+ALLOWED_HOSTS = _settings.ALLOWED_HOSTS
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'elmusetest.herokuapp.com',]
 
 
 # Application definition
