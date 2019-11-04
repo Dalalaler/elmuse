76767ce13e4115d00ea66a14ab2a6b4455d5ebfc
repatik <dalalaler@gmail.com>
2019-11-04 19:43:20 +0300

    Removed excess views, renamed home app to index app

--- src/index/views.py
+++ src/index/views.py
@@ -9,15 +9,6 @@ def elmuse(request):
     return render(request, "elmuse.html",)
 
 
-def index(request):
-    return render(request, "index.html",)
 
-
-def eng_index(request):
-    return render(request, "eng.html",)
-
-
-def ru_index(request):
-    return render(request, "ru.html",)
 
 # Create your views here.
