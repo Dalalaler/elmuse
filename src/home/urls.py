790df15a3c8ac74ed1c2a8bd9ab96d04f0f4ae6a
repatik <dalalaler@gmail.com>
2019-11-04 19:36:50 +0300

    Removed excess templates

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,9 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("eng/", views.eng_index, name='eng_index'),
-    path("ru/", views.ru_index, name='ru_index'),
-    path("elmuse/", views.elmuse, name='elmuse'),
-
+    path("", views.elmuse, name='elmuse'),
 ]
